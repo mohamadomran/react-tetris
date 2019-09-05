3eacc75b581d76088b640051069c08fdb86e6e3b
Mohamad Omran <mohamad1_3omran@hotmail.com>
2019-09-06 00:40:05 +0300

    Styles for Components

--- src/components/Cell.js
+++ src/components/Cell.js
@@ -1,5 +1,12 @@
 import React from "react";
+import { StyledCell } from "./styles/StyledCell";
 
-const Cell = ({ type }) => <div>cell</div>;
+import { TETROMINOS } from "../tetrominos";
+
+const Cell = ({ type }) => (
+  <StyledCell type={"L"} color={TETROMINOS[type].color}>
+    cell
+  </StyledCell>
+);
 
 export default Cell;
--- src/components/Tetris.js
+++ src/components/Tetris.js
@@ -2,6 +2,11 @@ import React from "react";
 
 import { createStage } from "../gameHelpers";
 
+import {
+  StyledTetrisWrapper,
+  StyledTetris
+} from "./styles/StyledTetrisWrapper";
+
 //Components
 import Stage from "./Stage";
 import Display from "./Display";
@@ -9,17 +14,19 @@ import StartButton from "./StartButton";
 
 const Tetris = () => {
   return (
-    <div>
-      <Stage stage={createStage()} />
-      <aside>
-        <div>
-          <Display text="Score" />
-          <Display text="Rows" />
-          <Display text="Level" />
-        </div>
-        <StartButton />
-      </aside>
-    </div>
+    <StyledTetrisWrapper>
+      <StyledTetris>
+        <Stage stage={createStage()} />
+        <aside>
+          <div>
+            <Display text="Score" />
+            <Display text="Rows" />
+            <Display text="Level" />
+          </div>
+          <StartButton />
+        </aside>
+      </StyledTetris>
+    </StyledTetrisWrapper>
   );
 };
 
